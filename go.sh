7f950f1688b58e1e94fade047c1936e285ab4ea5
Ronald McCollam <mccollam@gmail.com>
2016-05-24 11:22:15 -0400

    Add a light window manager, tweak chromium flags

--- go.sh
+++ go.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 startx &
-DISPLAY=:0 chromium-browser --app=http://resin.io
+export DISPLAY=:0
+
+# Prevent display from sleeping:
+xset s off
+
+# Start a minimal window manager so chromium can deal with fullscreen properly
+lwm &
+
+# 'no-sandbox' flag needed due to running as root
+chromium-browser --no-sandbox --user-data-dir --start-fullscreen --app=http://resin.io
